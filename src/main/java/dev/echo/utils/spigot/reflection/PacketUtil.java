577af87d2c68e3f555f81384f17720ae51dbc06c
Echoed <69612342+Echomonic@users.noreply.github.com>
2022-11-25 12:46:46 -0500

    Changed something

--- src/main/java/dev/echo/utils/spigot/reflection/PacketUtil.java
+++ src/main/java/dev/echo/utils/spigot/reflection/PacketUtil.java
@@ -8,44 +8,37 @@ import org.bukkit.entity.Player;
 public class PacketUtil {
 
     public static void sendPacket(Player player, Object packet) {
-        try {
-            Object handle  = player.getClass().getMethod("getHandle").invoke(player);
-            Object playerConnection = ReflectionUtils.getConnection(player);
-            playerConnection.getClass().getMethod("sendPacket", getNMSClass("Packet")).invoke(playerConnection, packet);
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        //Doing this cause why not lol.
+        ReflectionUtils.sendPacket(player, packet);
     }
-    public static void sendPacket(Object packet){
-        for(Player player : Bukkit.getOnlinePlayers()) sendPacket(player,packet);
+
+    public static void sendPacket(Object packet) {
+        for (Player player : Bukkit.getOnlinePlayers()) sendPacket(player, packet);
     }
-    public static void sendPacketNotFor(Player player, Object packet){
+
+    public static void sendPacketNotFor(Player player, Object packet) {
         for (Player targets : Bukkit.getOnlinePlayers()) {
             if (targets.getUniqueId().toString().equals(player.getUniqueId().toString())) continue;
-            sendPacket(targets,packet);
+            sendPacket(targets, packet);
         }
     }
+
     public static Class<?> getNMSClass(String name) {
         String version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
         try {
             return Class.forName("net.minecraft.server." + version + "." + name);
-        }
-
-        catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
             return null;
         }
 
     }
+
     public static Class<?> getCraftBukkitClass(String name) {
         String version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
         try {
             return Class.forName("org.bukkit.craftbukkit." + version + "." + name);
-        }
-
-        catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
             return null;
         }
